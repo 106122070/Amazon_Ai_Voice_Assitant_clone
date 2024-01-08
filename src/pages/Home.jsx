68f7be7bd11ca06d333d2da662986159f6e1f271
KISHORE V <131111608+106122070@users.noreply.github.com>
2024-01-08 20:32:14 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,14 +17,30 @@ import { fireAuth } from '../utils/firebase';
 import { signOut, onAuthStateChanged } from 'firebase/auth';
 import Footer from '../Components/Footer';
 
+
 const Home = () => {
+
+  const myRef = useRef();
+  
+  const [visibilitystyles,setvisibilitystyles]  = useState(false);
+
+useEffect(()=>{
+ console.log("success",visibilitystyles);
+  const observer = new IntersectionObserver((entries)=>{
+    const entry = entries[0];
+    setvisibilitystyles(entry.isIntersecting);
+  });
+observer.observe(myRef.current);
+},[visibilitystyles]);
+
   const alankey = "ae13481cb56b6adb737e8fd4a03c8a082e956eca572e1d8b807a3e2338fdd0dc/stage";
-  const cartmoneycollections = collection(firestore1,'cashcollections')
-    const [data1,setdata1] = useState([]);
-    const [cartamountdata,setcartamountdata] = useState([]);
-    const cartcount = useSelector((state) => state.cart.cartcount);
-const [listcategory,setlistcategory] = useState([]);
-const username = localStorage.getItem('username');
+  
+      const cartmoneycollections = collection(firestore1,'cashcollections')
+      const [data1,setdata1] = useState([]);
+      const [cartamountdata,setcartamountdata] = useState([]);
+      const cartcount = useSelector((state) => state.cart.cartcount);
+      const [listcategory,setlistcategory] = useState([]);
+      const username = localStorage.getItem('username');
 
     const fetchdata1 = () =>{
        setdata1(data.datas);
@@ -36,6 +52,7 @@ const username = localStorage.getItem('username');
         fetchdata1();
         setlistcategory(["jewelery","men's clothing","women's clothing","electronics","watch","accessories","laptop","mobile","tv"].sort(() => Math. random() - 0.5));
     },[]);
+
 
     const slicerdata1 = (category) =>{
         const lop1= data1.filter((data) => data.category == category).slice(0,4); 
@@ -57,8 +74,6 @@ const username = localStorage.getItem('username');
       return ()=>{
         cartmoneystore();
     }});
-
-    const myref = useRef();
 
     const scrollToTop = () =>{ 
       const scrollheight = document.documentElement.scrollHeight - window.scrollY;
@@ -144,7 +159,6 @@ if(fireAuth.currentUser!=null && count==1){
 else if( fireAuth.currentUser==null){
  count =0;
 }
-
     }, []);
 
   return (
@@ -162,13 +176,16 @@ else if( fireAuth.currentUser==null){
 
        {
        
-        listcategory.map((catedata,index) => {
-            return   <AnimatePresence><motion.div initial={{ opacity: 0, scale: 0 }}
+        listcategory.map((catedata,cindex) => {
+            return   <AnimatePresence>
+              <motion.div 
+            initial={{ opacity: 0, scale: 0 }}
             animate={{ opacity: 1, scale:1 }}
-            exit={{ opacity: 0, scale:0}} transition={{ type:'spring',duration: 2.5,stiffness:300,damping:50 }}>  <div className="home-product-row1" key={index}>   {
+            exit={{ opacity: 0, scale:0}} transition={{ type:'spring',duration: 2.5,stiffness:300,damping:50 }}> 
+             <div  className="home-product-row1" key={cindex}>   {
                 slicerdata1(catedata).map((d1,index) =>{
                     if(index < 4){
-                        return <Product imgsrc={d1.image}  id={d1.id} rating={d1.rating.rate} price={d1.price} proinfo={d1.title} prodescription = {d1.description}/>     
+                        return ( <Product imgsrc={d1.image}  id={d1.id} rating={d1.rating.rate} price={d1.price} proinfo={d1.title} prodescription = {d1.description}/>   )
                     }
                 })
                }</div> </motion.div> </AnimatePresence>
@@ -177,7 +194,7 @@ else if( fireAuth.currentUser==null){
        }
       </div>
       <div className="btn-back-to-top">
-       <button className='back-btn' onClick={()=>scrollToTop()}>Back To Top</button>
+       <button className="back-btn show-btn" ref={myRef} style={{opacity:`${visibilitystyles ? 1:0}`}} onClick={()=>scrollToTop()}  >Back To Top</button>
       </div>
       </div>
       <Footer/>
@@ -243,7 +260,8 @@ const Homestyles = styled.div`
             color: whitesmoke;
             border-radius: 50px;
             cursor:pointer;
-            transition:.3s ease-in-out;
+            transition:.55s ease-in-out;
+
           }
 
         .back-btn:hover{
